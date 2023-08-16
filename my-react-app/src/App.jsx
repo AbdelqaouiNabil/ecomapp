73826417fd5a327ed580b2521434875f46c79346
younes-biloul <biloulyounes@gmail.com>
2023-08-16 17:12:09 +0100

    mehdi changes 16-08

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,25 +1,16 @@
-import {  Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
-import Login from "./Login";
+import Login from "./pages/login/login";
 function App() {
+    return (
+        <>
 
-  return (
-    <>
-      <div className='max-w-xl6 mx-auto'>
-       <h1> Hello </h1>
-       <h2> Back End Developer </h2>
-       <Link to="/">Home</Link>
-       <Link to="/login">Login</Link>
-       <Link to="/register">Register</Link>
-       <div>
-        <Routes>
-        <Route path="Login" element={<Login />} />
-        </Routes>
-       </div>
-      </div>
-     
-    </>
-  )
+            <Routes>
+                <Route path="Login" element={<Login />} />
+            </Routes>
+
+        </>
+    );
 }
 
-export default App
+export default App;
